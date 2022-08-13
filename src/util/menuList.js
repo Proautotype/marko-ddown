e8d7e9cde7324344848d2c5fe3ffac9e52058254
Ampomah Winston <winstyngyen@gmail.com>
2022-08-13 15:48:29 +0000

    8/13/22 first commit

---
+++ src/util/menuList.js
@@ -0,0 +1,12 @@
+module.exports = [
+    "Jollof",
+    "Rice + Beans", 
+    "Banku", 
+    "Cashew Juice + Pie", 
+    "Yam + gravies",
+    "Potato chips + Cook",
+    "Waakye + Shito",
+    "Fufu + Groundnut Soup",
+    "Duck meat + beaf stew",
+    "Kenkey + Meat",
+]
